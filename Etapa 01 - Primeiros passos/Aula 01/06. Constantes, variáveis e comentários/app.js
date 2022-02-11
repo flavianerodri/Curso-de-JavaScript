ced3a02c42cbf56f93468a038ea042c4cba7fdba
Flaviane Rodrigues <flaviane.tania@outlook.com>
2022-02-10 23:29:18 -0300

    Constantes, variáveis e comentários

---
+++ Etapa 01 - Primeiros passos/Aula 01/06. Constantes, variáveis e comentários/app.js
@@ -0,0 +1,20 @@
+/*
+
+Comentário multi linha
+
+*/
+
+// Comentário 
+
+let age = 31
+let currentYear = 2022
+
+console.log(age, currentYear)
+
+age = 32
+
+console.log(age)
+
+const points = 100
+
+console.log(points)
