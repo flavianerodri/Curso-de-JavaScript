51b192b621a7b5a6b479733f65ac63f88bde97a4
Flaviane Rodrigues <flaviane.tania@outlook.com>
2022-02-12 23:54:55 -0300

    Aula 02-04 Strings

---
+++ Etapa 01 - Primeiros passos/Aula 02/03. Strings/app.js
@@ -0,0 +1,30 @@
+// strings
+console.log('hello, world')
+
+const email = 'leon@rogermelo.com.br'
+
+console.log(email)
+
+// concatenar strings
+const firstName = 'Nathan'
+const lastName = 'Drake'
+const fullName = firstName + ' ' + lastName
+
+console.log(fullName)
+
+// acessando caracteres 
+console.log(fullName[2])
+
+// comprimento de uma string
+console.log(fullName.length)
+
+// métodos de string
+console.log(fullName.toUpperCase())
+
+const result = fullName.toLowerCase()
+
+console.log(result, fullName)
+
+const index = email.indexOf('@')
+
+console.log(index)
